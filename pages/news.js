2d92e848a06e2bb4cf314b7a59884a4b44c82777
James Marshall <jameswhmarshallactions@gmail.com>
2023-07-17 19:26:57 +1000

    updated News page to resolve deployment error

---
+++ pages/news.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+export default function News() {
+  return (
+    <Layout>
+      <h1>EDM News</h1>
+      <p>High volume, low effort EDM news</p>
+    </Layout>
+  );
+}
