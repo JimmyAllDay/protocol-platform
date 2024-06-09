0aaed25ad619d98dbdf254564869dfacfee1fa7e
James Marshall <jameswhmarshallactions@gmail.com>
2024-06-09 11:16:54 +1000

    updated google analytics

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
 
 import Head from 'next/head';
-import Script from 'next/script';
+import GoogleAnalytics from './analytics/GoogleAnalytics';
 
 import Header from './header/Header';
 import Footer from './footer/Footer';
@@ -16,50 +16,14 @@ const Layout = ({ title, children }) => {
   const [showBanner, setShowBanner] = useState(true);
   const { loading } = useContext(LoadingContext);
 
-  const hotjarId = 5016933;
-  const hotjarSv = 6;
-  const googleAnalyticsId = 'G-MCYH6WMHBX';
-
   return (
     <>
-      <Script
-        strategy="beforeInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
-      />
-      <Script
-        id="google-analytics-script"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${googleAnalyticsId}');
-          `,
-        }}
-      />
-      <Script
-        id="hotjar-script"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:${hotjarId},hjsv:${hotjarSv}};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `,
-        }}
-      />
       <Head>
         <title>{title ? title + ' - Pro.ground' : 'Pro.ground'}</title>
         <meta name="description" content="Ecommerce Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <GoogleAnalytics />
       <ToastContainer
         position="bottom-center"
         autoClose={5000}
