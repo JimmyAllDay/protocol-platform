43109fcc3e3b937d33ca2b6eb676f30b7e4d6a19
James Marshall <jameswhmarshallactions@gmail.com>
2024-06-09 11:26:46 +1000

    updated hotjar and google analytics implementation

---
+++ components/analytics/HotJar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Script from 'next/script';
+
+const HotJar = () => {
+  return (
+    <>
+      <Script id="hot-jar-script" strategy="lazyOnload">
+        {`
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:5016933,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+      </Script>
+    </>
+  );
+};
+
+export default HotJar;
