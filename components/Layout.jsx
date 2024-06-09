5782d2279baeece97e17a716cf402e53a0cfa6ac
James Marshall <jameswhmarshallactions@gmail.com>
2024-06-09 10:03:06 +1000

    added google analytics

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useContext, useEffect } from 'react';
 
 import Head from 'next/head';
+import Script from 'next/script';
+
 import Header from './header/Header';
 import Footer from './footer/Footer';
 import Banner from 'components/header/Banner';
@@ -16,12 +18,28 @@ const Layout = ({ title, children }) => {
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLEANALYTICSID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', ${process.env.GOOGLEANALYTICSID});
+          `,
+        }}
+      />
       <Head>
         <title>{title ? title + ' - Pro.ground' : 'Pro.ground'}</title>
         <meta name="description" content="Ecommerce Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <ToastContainer
         position="bottom-center"
         autoClose={5000}
--- next.config.js
+++ next.config.js
@@ -17,6 +17,7 @@ const nextConfig = {
   env: {
     HOTJARID: process.env.HOTJARID,
     HOTJARSV: process.env.HOTJARSV,
+    GOOGLEANALYTICSID: process.env.GOOGLEANALYTICSID,
     BASE_URL: process.env.BASE_URL,
     RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
     RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
