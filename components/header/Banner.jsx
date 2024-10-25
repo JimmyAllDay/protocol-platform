ec7eed76fe1138931e0207d2f604ae0f9e3143c3
James Marshall <jameswhmarshallactions@gmail.com>
2024-10-26 10:28:35 +1100

    adjusted banner animation, some layout updates

--- components/header/Banner.jsx
+++ components/header/Banner.jsx
@@ -4,10 +4,13 @@ export default function Banner() {
   const bannerMessage = `If you're a DJ and want to play at a Pro.ground event, sign up, give us your details and upload a mix. We'll be in touch.`;
 
   return (
-    <div className="bg-primary dark:bg-primaryDark flex items-center justify-center text-center w-screen">
-      <div className="marquee w-full">
-        <div className="marquee-track w-full">
-          <p className="text-accent dark:text-accent2Dark marquee-content">
+    <div className="bg-primary dark:bg-primaryDark flex items-center justify-center text-center w-screen overflow-hidden">
+      <div className="marquee w-full overflow-hidden whitespace-nowrap">
+        <div className="marquee-track">
+          <p className="text-accent dark:text-accent2Dark marque-content">
+            {bannerMessage}
+          </p>
+          <p className="text-accent dark:text-accent2Dark marque-content invisible">
             {bannerMessage}
           </p>
         </div>
